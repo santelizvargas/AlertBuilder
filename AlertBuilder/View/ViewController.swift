4328f15a65be47e164f51524fedb981c1dc2a1f7
santelizvargas <ushija6@gmail.com>
2024-12-26 02:51:13 -0600

    Chore: refactor

--- AlertBuilder/View/ViewController.swift
+++ AlertBuilder/View/ViewController.swift
@@ -7,10 +7,53 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
-    }
+// MARK: - Constants
+
+private enum AlertMessages {
+    static let internalAlert = "This is a custom message"
+    static let presenterAlert = "This is an alert from presenter"
+    static let simpleAlert = "This is a simple alert"
 }
 
+final class ViewController: UIViewController {
+    
+    // MARK: - Alert Builders
+    
+    private lazy var internalAlert: UIAlertController = {
+        let doneAction = AlertAction(title: "Done")
+        
+        return AlertBuilder()
+            .setMessage(AlertMessages.internalAlert)
+            .addAction(doneAction)
+            .setPreferredStyle(.actionSheet)
+            .build()
+    }()
+    
+    private lazy var presenterAlert: UIAlertController = {
+        AlertBuilder()
+            .setMessage(AlertMessages.presenterAlert)
+            .build()
+    }()
+    
+    // MARK: - Alert Presentation
+    
+    private func presentInternalAlert() {
+        navigationController?.present(internalAlert, animated: true)
+    }
+    
+    private func presentPresenterAlert() {
+        AlertPresenter.present(presenterAlert)
+    }
+    
+    private func presentSimpleAlert() {
+        AlertPresenter.present(AlertMessages.simpleAlert)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction private func showAlert() {
+//        presentInternalAlert()
+        presentSimpleAlert()
+//        presentPresenterAlert()
+    }
+}
